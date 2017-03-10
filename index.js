6d8a55329bea9d1efb94a684dcca5e39adcf6659
teldridge11 <tom.eldridge@hotmail.com>
2017-03-10 13:25:26 -0500

    Added webcrawler route

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(app.router);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,6 +18,29 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+app.post('/webcrawler', function(req, res){
+  console.log("Webcrawler");
+  /*
+  var python = require('child_process').spawn(
+  'python',
+  ["WebCrawler/Webcrawler.py"
+  , req.source
+  , req.method
+  , req.nodeCount
+  , req.depth
+  , req.keyword]
+  );
+  var output = "";
+  python.stdout.on('data', function(data){ output += data });
+  python.on('close', function(code){ 
+    if (code !== 0) {  
+        return res.send(500, code); 
+    }
+    return res.send(200, output);
+  });
+  */
 });
 
 
