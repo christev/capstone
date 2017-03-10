0d231a09710232ccb9f65ac3e5c51c727dcc8d68
teldridge11 <tom.eldridge@hotmail.com>
2017-03-10 14:01:38 -0500

    Removed body parser

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
 app.use(app.router);
 
 app.set('port', (process.env.PORT || 5000));
