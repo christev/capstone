7f6b06d41b7086f51869a7decae9108668163c69
teldridge11 <tom.eldridge@hotmail.com>
2017-03-15 16:27:59 -0400

    Debug output

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/webcrawler', function(req, res){
   python.stdout.on('data', function(data){ output += data });
   python.on('close', function(code){
     if (code !== 0) {
-        return res.send(500, code);
+        return res.send(500, output);
     }
     console.log(code);
     console.log(output);
